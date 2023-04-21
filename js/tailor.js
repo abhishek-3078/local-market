9ee64f82176fe0f199c1042bc0b39bab95b25719
Abhishek <12213078@nitkkr.ac.in>
2023-04-22 00:23:08 +0530

    add tailor

---
+++ js/tailor.js
@@ -0,0 +1,6 @@
+const measurement=document.querySelector(".measurements")
+function addMeasurement(){
+    if(measurement.style.display="none")
+        measurement.style.display="flex"
+    else measurement.style.display="none"
+}
