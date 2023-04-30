a609fded7b65855191bd603d10472dce30e8a4a2
Abhishek <12213078@nitkkr.ac.in>
2023-04-30 14:47:49 +0530

    add index feature

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const nextBtn=document.querySelector('.next')
+const prevBtn=document.querySelector('.prev')
+const container=document.querySelector('.categories-container')
+const box=document.querySelector('.category')
+let counter=1
+container.scrollLeft=0
+boxStyle=getComputedStyle(box)
+nextBtn.onclick=(e)=>{
+    container.scrollLeft+=box.offsetWidth+ 2*parseInt(boxStyle.marginRight)
+    
+
+}
+prevBtn.onclick=(e)=>{
+    
+    container.scrollLeft-=box.offsetWidth+ 2*parseInt(boxStyle.marginRight)
+}
