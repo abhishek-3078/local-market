1d04ed24be455d1710fc79903d88fab467afd815
Abhishek <12213078@nitkkr.ac.in>
2023-05-06 14:21:36 +0530

    bug fixes

---
+++ js/services.js
@@ -0,0 +1,30 @@
+const addBtn=document.querySelectorAll('.cartBtn')
+function addToCart(name,price){
+    console.log(name,price)
+    let item={}
+    localStorage.removeItem(name)
+//     if(localStorage.getItem(name)){
+//          item=JSON.parse(localStorage.getItem(name))
+//         item.qty++;
+//     }else{
+//     item={
+//         price:price,
+//         qty:1
+//     }
+// }
+//     item=JSON.stringify(item)
+//     console.log(item)
+
+//     localStorage.setItem(name,item)
+    
+    
+}
+addBtn.forEach((btn)=>{
+    btn.addEventListener('click',(event)=>{
+       
+        let bx=event.target.closest('li')
+        
+        addToCart(bx.querySelector('h1').innerText,bx.querySelector('p').innerText)
+       
+    })
+})
